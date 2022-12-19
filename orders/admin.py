e276c0fa4282833c17bd5aaa729d5da58d77643a
Sefa Sertkaya <sefqi52@gmail.com>
2022-12-20 02:22:48 +0300

    ok

--- orders/admin.py
+++ orders/admin.py
@@ -10,10 +10,10 @@ class OrderProductline(admin.TabularInline):
     extra = 0
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "order_number", "email", "phone", "state", "city", "order_total", "status", "is_ordered"]
+    list_display = ["first_name", "order_number", "email", "phone", "state", "city", "order_total", "status", "is_ordered"]
     list_filter = ["status"]
-    readonly_fields = ("order_number", "email", "user", "address_line", "state", "city", "phone", "first_name", "ip", "last_name", "phone", "order_total")
-    search_fields = ["order_number", "first_name", "last_name", "phone", "email"]
+    readonly_fields = ("order_number", "email", "user", "address_line", "state", "city", "phone", "first_name", "ip", "phone", "order_total")
+    search_fields = ["order_number", "first_name", "phone", "email"]
     list_per_page = 20
     can_delete = False
     inlines = [OrderProductline]
