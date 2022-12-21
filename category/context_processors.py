d28f0da8d5a1177d65c51708e5db3d12d449df45
Sefa Sertkaya <sefqi52@gmail.com>
2022-12-22 02:45:47 +0300

    ayarlar

--- category/context_processors.py
+++ category/context_processors.py
@@ -2,7 +2,8 @@ from .models import Category, Setting
 
 def menu_links(request):
     links = Category.objects.all()
-    return dict(links=links)
+    ayarlar = Setting.objects.all()
+    return dict(links=links, ayarlar=ayarlar)
 
 
 def context(request):
