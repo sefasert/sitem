3be6048e6e0b96f60310283a413c97df8336a5fb
Sefa Sertkaya <sefqi52@gmail.com>
2022-12-22 03:09:25 +0300

    form ayarları

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, username, email, password=None):
         if not email:
             raise ValueError("User must have an email address")
 
@@ -22,7 +22,7 @@ class MyAccountManager(BaseUserManager):
         return user
 
 
-    def create_superuser(self, first_name, last_name, email, username, password=None):
+    def create_superuser(self, first_name, email, username, password=None):
         user = self.create_user(
             email       = self.normalize_email(email),
             username    = username,
