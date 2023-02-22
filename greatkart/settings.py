3f6d2e2f3bb59fc03ecdd3612ecb908eb8ab76ee
Sefa Sertkaya <sefqi52@gmail.com>
2023-02-22 23:05:05 +0300

    ok

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     "category",
     "accounts",
     "store",
@@ -60,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'greatkart.urls'
@@ -142,8 +143,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = ["greatkart/static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
@@ -170,8 +171,6 @@ EMAIL_HOST_USER = "enginelektronikinfo@gmail.com"
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Engin Elektronik <enginelektronikinfo@gmail.com>"
-
-
 
 
 CSRF_TRUSTED_ORIGINS = ["https://sitem-production.up.railway.app", "https://www.enginelektronik.xyz", "http://www.enginelektronik.xyz"]
